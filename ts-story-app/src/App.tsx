5630cdcf08021886954cc8169bdf342197c5d5ba
Emelyne12 <ingabiremelyne1234@gmail.com>
2026-04-08 14:31:37 +0200

    first commit

---
+++ ts-story-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-blue font-bold text-4xl'>Tailwind is successfully installed!</h1>
+
+    </>
+  )
+}
+
+export default App
